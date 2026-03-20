67dc633307b37245bceffcb42e847a4db86e1949
Anis SAJED <sajed.anis@free.fr>
2026-03-20 19:22:02 +0100

    increment

--- client.ts
+++ client.ts
@@ -1,8 +1,1 @@
 export {buildApi} from "./lib/api.js";
-
-export type {Requester, Api} from "./lib/api.types.js";
-export type {
-  DestOpts,
-  Client,
-} from "./lib/transport/client/types.js";
-export type {RawConfigLoader} from "./lib/config/load-config.types.js";
