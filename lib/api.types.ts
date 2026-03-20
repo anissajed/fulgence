c9b0915387c33bfd3223577693beab3227cf8d4c
Anis SAJED <sajed.anis@free.fr>
2026-03-20 19:42:46 +0100

    increment

--- lib/api.types.ts
+++
@@ -1,9 +0,0 @@
-import {GetRawConfigOpts} from "./config/load-config.types.js";
-
-export type Requester = (request_args: unknown, ...rest: unknown[]) => Promise<unknown>;
-export type Api = Record<string, Requester>;
-
-type BuildApiOpts = {local_module_name?: string} & GetRawConfigOpts;
-export type BuildApi = (opts: BuildApiOpts) => Promise<Api>;
-
-export type LocalTaskCaller = (opts) => Promise<Requester>;
