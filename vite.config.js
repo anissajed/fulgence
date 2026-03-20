36ab9fafdcf61caa8d55a730c06b77727edf4208
Anis SAJED <sajed.anis@free.fr>
2026-03-20 18:54:48 +0100

    increment

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
-import {defineConfig, mergeConfig} from "vite";
+import {defineConfig} from "vite";
 import {resolve} from "path";
 import dts from "vite-plugin-dts";
 import nodeExternals from "rollup-plugin-node-externals";
 
 const __dirname = import.meta.dirname;
 
-const configuredDtsPlugin = ({include}) => dts({
-  include,
-  insertTypesEntry: true,
-})
-
-const base_config = defineConfig({
+export const node_config = defineConfig({
   build: {
     lib: {
       formats: ["es", "cjs"],
       fileName: (format, entryName) => `${entryName}.${format === "cjs" ? "cjs" : "mjs"}`,
+      entry: {
+        "transport/client/default/index": resolve(__dirname, "lib/transport/client/default/index.ts"),
+        "transport/server/default/index": resolve(__dirname, "lib/transport/server/default/index.ts"),
+      },
     },
     minify: false,
 
@@ -23,19 +22,11 @@ const base_config = defineConfig({
   output: {
     exports: "named",
   },
-});
-
-export const node_config = mergeConfig(base_config, {
-  build: {
-    lib: {
-      entry: {
-        "transport/client/default/index": resolve(__dirname, "lib/transport/client/default/index.ts"),
-        "transport/server/default/index": resolve(__dirname, "lib/transport/server/default/index.ts"),
-      },
-    },
-  },
   plugins: [
     {...nodeExternals(), enforce: "pre"},
-    configuredDtsPlugin({include: ["lib", "client.ts", "index.ts"]}),
+    dts({
+      include: ["lib", "client.ts", "index.ts"],
+      insertTypesEntry: true,
+    }),
   ],
 });
