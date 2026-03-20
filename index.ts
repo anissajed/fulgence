f1bb8b576f07e0036445fb898ba074f5e1872999
Anis SAJED <sajed.anis@free.fr>
2026-03-20 19:16:30 +0100

    increment

--- index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./client.js";
-export type * from "./client.js";
-
-export type {
-  ServerInstance,
-  ServerOpts,
-  Server,
-} from "./lib/transport/server/types.js";
-export type {OnInitTask, OnDoTask} from "./lib/tasks-lifecycle/types.js";
--- lib/transport/server/types.ts
+++
@@ -1,9 +0,0 @@
-import {Api} from "../../api.types.js";
-
-export type ServerInstance = unknown;
-export type ServerOpts<T = unknown> = {
-  api: Api;
-  name: string;
-  opts?: T;
-};
-export type Server<T = unknown> = (opts: ServerOpts<T>) => Promise<ServerInstance>;
