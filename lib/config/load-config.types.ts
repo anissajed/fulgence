1f7201af2327764e6dcfe3b8168f840d9608923d
Anis SAJED <sajed.anis@free.fr>
2026-04-11 02:08:25 +0200

    docs(lib): :memo: better types for buildApi

--- lib/config/load-config.types.ts
+++ lib/config/load-config.types.ts
@@ -21,12 +21,20 @@ export type BuildConfig = (opts: BuildConfigOpts) => Config;
 
 export type RawConfigLoader = (config_path: string) => Promise<RawConfig>;
 
+/*
 type GetRawConfigOptsConfigPath = {
   config_path: string;
 };
 type GetRawConfigOptsConfig = {
   config: RawConfig;
 };
-export type GetRawConfigOpts = GetRawConfigOptsConfigPath | GetRawConfigOptsConfig;
+
+// This should be the most proper type to be used. But since TS can't understand this in the clients codes, we will export another simpler one. We keep this "more human readable version" here for documentation.
+type GetRawConfigOpts = GetRawConfigOptsConfigPath | GetRawConfigOptsConfig | (GetRawConfigOptsConfigPath & GetRawConfigOptsConfig);
+*/
+export interface GetRawConfigOpts {
+  config_path?: string;
+  config?: RawConfig;
+};
 export type GetRawConfig = (opts: GetRawConfigOpts) => RawConfig;
 
