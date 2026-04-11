5aae68e1266b65aeb71da568a9f840dfaa27b250
Anis SAJED <sajed.anis@free.fr>
2026-04-11 02:40:06 +0200

    temp nestjs integration v3: alter lib interface: add local to onInitTask and onDoTask

--- lib/tasks-lifecycle/types.ts
+++ lib/tasks-lifecycle/types.ts
@@ -1,5 +1,10 @@
 import {Api} from "../api.types.js";
 
 type Task = any;
-export type OnInitTask = (opts: {module, api: Api}) => Promise<Task>;
-export type OnDoTask = (opts: {task: Task, input: unknown, api: Api}) => Promise<unknown>;
+export type OnInitTask = (opts: {module, api: Api, local: boolean}) => Promise<Task>;
+export type OnDoTask = (opts: {
+  task: Task,
+  input: unknown,
+  api: Api,
+  local: boolean,
+}) => Promise<unknown>;
